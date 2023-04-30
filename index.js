b2a0f57df3ac531a0cde64e923106194deea7a90
Elian <eliangerardiso@gmail.com>
2023-04-30 00:16:37 -0600

    Added supported languages

--- index.js
+++ index.js
@@ -8,6 +8,64 @@ const path = require('path');
  * @param {string} text - The text to convert to audio.
  * @param {string} fileName - The name of the audio file to create (can include a relative path).
  * @param {string} [language='en'] - The language of the text (ISO 639-1 code) (optional).
+* 
+ * @supportedLanguages
+ *  - af - Afrikaans
+ *  - sq - Albanian
+ *  - de - German
+ *  - ar - Arabic
+ *  - bn - Bengali
+ *  - my - Burmese
+ *  - bs - Bosnian
+ *  - bg - Bulgarian
+ *  - km - Cambodian
+ *  - kn - Kannada
+ *  - ca - Catalan
+ *  - cs - Czech
+ *  - zh - Simplified Chinese
+ *  - zh-TW - Traditional Chinese
+ *  - si - Sinhalese
+ *  - ko - Korean
+ *  - hr - Croatian
+ *  - da - Danish
+ *  - sk - Slovak
+ *  - es - Spanish
+ *  - et - Estonian
+ *  - fi - Finnish
+ *  - fr - French
+ *  - el - Greek
+ *  - gu - Gujarati
+ *  - hi - Hindi
+ *  - nl - Dutch
+ *  - hu - Hungarian
+ *  - id - Indonesian
+ *  - en - English
+ *  - is - Icelandic
+ *  - it - Italian
+ *  - ja - Japanese
+ *  - la - Latin
+ *  - lv - Latvian
+ *  - ml - Malayalam
+ *  - ms - Malay
+ *  - mr - Marathi
+ *  - ne - Nepali
+ *  - no - Norwegian
+ *  - pl - Polish
+ *  - pt - Portuguese
+ *  - ro - Romanian
+ *  - ru - Russian
+ *  - sr - Serbian
+ *  - sw - Swahili
+ *  - sv - Swedish
+ *  - su - Sundanese
+ *  - tl - Tagalog
+ *  - th - Thai
+ *  - ta - Tamil
+ *  - te - Telugu
+ *  - tr - Turkish
+ *  - uk - Ukrainian
+ *  - ur - Urdu
+ *  - vi - Vietnamese
  */
 function createAudioFile(text, fileName, language = 'en') {
   const filePath = path.resolve(`${fileName}.mp3`);
@@ -30,6 +88,64 @@ function createAudioFile(text, fileName, language = 'en') {
  * @param {string} text - The text to convert to audio.
  * @param {string} [language='en'] - The language of the text (ISO 639-1 code) (optional).
  * @returns {Promise<Buffer>} A promise that resolves with the mp3 audio buffer.
+ * 
+ * @supportedLanguages
+ *  - af - Afrikaans
+ *  - sq - Albanian
+ *  - de - German
+ *  - ar - Arabic
+ *  - bn - Bengali
+ *  - my - Burmese
+ *  - bs - Bosnian
+ *  - bg - Bulgarian
+ *  - km - Cambodian
+ *  - kn - Kannada
+ *  - ca - Catalan
+ *  - cs - Czech
+ *  - zh - Simplified Chinese
+ *  - zh-TW - Traditional Chinese
+ *  - si - Sinhalese
+ *  - ko - Korean
+ *  - hr - Croatian
+ *  - da - Danish
+ *  - sk - Slovak
+ *  - es - Spanish
+ *  - et - Estonian
+ *  - fi - Finnish
+ *  - fr - French
+ *  - el - Greek
+ *  - gu - Gujarati
+ *  - hi - Hindi
+ *  - nl - Dutch
+ *  - hu - Hungarian
+ *  - id - Indonesian
+ *  - en - English
+ *  - is - Icelandic
+ *  - it - Italian
+ *  - ja - Japanese
+ *  - la - Latin
+ *  - lv - Latvian
+ *  - ml - Malayalam
+ *  - ms - Malay
+ *  - mr - Marathi
+ *  - ne - Nepali
+ *  - no - Norwegian
+ *  - pl - Polish
+ *  - pt - Portuguese
+ *  - ro - Romanian
+ *  - ru - Russian
+ *  - sr - Serbian
+ *  - sw - Swahili
+ *  - sv - Swedish
+ *  - su - Sundanese
+ *  - tl - Tagalog
+ *  - th - Thai
+ *  - ta - Tamil
+ *  - te - Telugu
+ *  - tr - Turkish
+ *  - uk - Ukrainian
+ *  - ur - Urdu
+ *  - vi - Vietnamese
  */
 function getAudioBuffer(text, language = 'en') {
   return googleTTS.getAllAudioBase64(text, { lang: language })
